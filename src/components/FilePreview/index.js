199b42d2a8527467cc9d7fe36c6d7d134a8fdd30
Andrew McLagan <andrewmclagan@gmail.com>
2019-07-24 16:52:53 +1000

    Bugs in FilePreview

--- src/components/FilePreview/index.js
+++ src/components/FilePreview/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import withTus from '../../lib/withTus'
-import FilePreview from './FilePreview.js'
+import React from "react";
+import withTus from "../../lib/withTus";
+import FilePreview from "./FilePreview.js";
 
 const WrappedFilePreview = ({ tus, ...props }) => {
-  const { upload, uploading } = tus
+  const { upload, uploading } = tus;
   if (!upload || uploading) {
-    return null
+    return null;
   }
-  return (<FilePreview upload={upload} {...props} />)
-}
+  return <FilePreview upload={upload} reset={tus.reset} {...props} />;
+};
 
-export default withTus(WrappedFilePreview)
+export default withTus(WrappedFilePreview);
