7febb712fdbae64a925b116790b6ad099fd54871
Andrew McLagan <andrewmclagan@gmail.com>
2019-08-28 13:24:44 +1000

    Upgrading dependancies

--- src/components/Tus/Input.js
+++ src/components/Tus/Input.js
@@ -1,68 +1,73 @@
-import React from 'react'
-import tus from 'tus-js-client'
-import { getAcceptedTypes } from '../../lib/acceptedTypes'
+import React from "react";
+import tus from "tus-js-client";
+import { getAcceptedTypes } from "../../lib/acceptedTypes";
 
 export class Input extends React.Component {
-  onStart = (upload) => {
+  onStart = upload => {
     if (this.props.onStart(upload) !== false) {
-      upload.start()
+      upload.start();
     }
-  }
+  };
 
-  onSuccess = (upload) => {
-    this.props.onSuccess(upload)
-  }
+  onSuccess = upload => {
+    this.props.onSuccess(upload);
+  };
 
-  onProgress = (upload, { uploaded, total }) => {
-    const percentage = Math.round(uploaded / total * 100)
-    this.props.onProgress(upload, { uploaded, total, percentage })
-  }
+  onProgress = (upload, { bytesSent, bytesTotal }) => {
+    const percentage = Math.round((bytesSent / bytesTotal) * 100);
+    console.log('bytesSent', bytesSent);
+    console.log('bytesTotal', bytesTotal);
+    console.log('percentage', percentage);
+    this.props.onProgress(upload, { bytesSent, bytesTotal, percentage });
+  };
 
   onError = (upload, error) => {
-    this.props.onError(upload, error)
-  }
+    this.props.onError(upload, error);
+  };
 
-  onChange = (event) => {
+  onChange = event => {
     if (event.target.files.length) {
-      const file = event.target.files[0]
-      const upload = this.createUpload(file)
-      this.onStart(upload)
+      const file = event.target.files[0];
+      const upload = this.createUpload(file);
+      this.onStart(upload);
     }
-  }
+  };
 
-  createUpload = (file) => {
+  createUpload = file => {
     const upload = new tus.Upload(file, {
       endpoint: this.props.endpoint,
-      retryDelays: [0, 3000, 5000],
+      retryDelays: [0, 3000, 5000, 8000],
       resume: false,
       metadata: {
         filename: file.name,
         filetype: file.type
       },
-      onError: (error) => this.onError(upload, error),
-      onProgress: (uploaded, total) => this.onProgress(upload, { uploaded, total }),
-      onSuccess: () => this.onSuccess(upload),
-    })
+      onError: error => this.onError(upload, error),
+      onProgress: (bytesSent, bytesTotal) =>
+        this.onProgress(upload, { bytesSent, bytesTotal }),
+      onSuccess: () => this.onSuccess(upload)
+    });
     return upload;
-  }
+  };
 
   render() {
-    const { name, allowedFileTypes, innerRef } = this.props
-    const accept = Array.isArray(allowedFileTypes) ?
-      allowedFileTypes.join() : getAcceptedTypes(allowedFileTypes);
+    const { name, allowedFileTypes, innerRef } = this.props;
+    const accept = Array.isArray(allowedFileTypes)
+      ? allowedFileTypes.join()
+      : getAcceptedTypes(allowedFileTypes);
     return (
       <input
         name={name}
-        style={{ display: 'none' }}
+        style={{ display: "none" }}
         type="file"
         accept={accept}
         onChange={this.onChange}
         ref={innerRef}
       />
-    )
+    );
   }
 }
 
-export default React.forwardRef((props, ref) =>
+export default React.forwardRef((props, ref) => (
   <Input innerRef={ref} {...props} />
-);
+));
