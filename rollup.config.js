b2c0fd1709c4d95ba7d756a8332f67e783869d9d
Andrew McLagan <andrewmclagan@gmail.com>
2019-09-19 14:07:49 +1000

    Refactoring build

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,31 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
-import pkg from './package.json'
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import resolve from "rollup-plugin-node-resolve";
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
+      format: "cjs",
       sourcemap: true
     },
     {
       file: pkg.module,
-      format: 'es',
+      format: "es",
       sourcemap: true
     }
   ],
   plugins: [
     external(),
-    postcss({
-      extract: true,
-      modules: false
-    }),
-    url(),
-    svgr(),
     babel({
-      exclude: 'node_modules/**'
+      exclude: "node_modules/**"
     }),
     resolve({
       browser: true
     }),
     commonjs()
   ]
-}
+};
